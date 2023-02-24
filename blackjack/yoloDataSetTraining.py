3c9bc83e2029c58f0a2fcfc735a91fe6a89d36e6
ramirez09r <33635574+ramirez09r@users.noreply.github.com>
2023-02-23 19:56:48 -0800

    Update yoloDataSetTraining.py

--- blackjack/yoloDataSetTraining.py
+++ blackjack/yoloDataSetTraining.py
@@ -5,7 +5,7 @@ import math
 import time
 import test
 
-# I hard coded these classes, there are many ways to do this, I just decided to do it the fu** it way
+# These are the classes used by the model
 
 classNames= ['10c', '10d', '10h', '10s',
              '2c', '2d', '2h', '2s',
@@ -30,14 +30,12 @@ classNames= ['10c', '10d', '10h', '10s',
 
 # Video import #Webcam import  *****UNCOMENT THIS BLOCK TO USE VIDEO FILE MAKE SURE TO COMENT OUT THE WEBCAM IMPORT BLOCK******
 # I uploaded some test videos on GitHub, but it should work on any mp4
-cap = cv2.VideoCapture('../Videos/blackjackL.mp4')
+cap = cv2.VideoCapture('blackjackL.mp4')
 
-# Load the YOLO trained model from Yolo-Weights fite (Check Github)
-model = YOLO("../Yolo-Weights/playingCards.pt")
+# Load the YOLO trained model 
+model = YOLO("CardModeNano.pt")
 
-# Use this after you create a Yolo-Weights folder to save your yolo file generated after you run your app 1st time
-# Makes it so you don't have to build model every time **MAKE SURE TO COMMENT OUT THE PREVIOUS model BLOCK
-# model = YOLO("../Yolo-Weights/yolov8n.pt")
+# model = YOLO("best.pt")
 
 
 # Loop to display images, add boxes and labels
@@ -55,27 +53,38 @@ while True:
             # print(x1, y1, x2, y2)
             # cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,255), 3)
             w, h = x2 - x1, y2 - y1
-            #cvzone.cornerRect(img, (x1, y1, w, h))
+            
+            
+            cvzone.cornerRect(img, (x1, y1, w, h))
 
-            # Confidence value display
+            # Confidence value
             conf = math.ceil((box.conf[0] * 100)) / 100
             #print(conf)
 
             # Class name (From hard coded classes, Doesn't work too good, but it works)
             cls = int(box.cls[0])
-            #cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1.8)
-
+            
+            #Uncoment this if you want to display the card value on the screen
+            #cvzone.putTextRect(img, f'{classNames[cls]}, (max(0, x1), max(35, y1)), scale=1.8)
+            
+            
+            #If the confidence level is greater than 0.5 the append the class to the hand list
             if conf > 0.5:
                 hand.append(classNames[cls])
 
 
-
+    #I used this to get rid of duplicates
+    # basically you turn the hand list into a unique set list
+    #THIS MIGHT BE AN ISSUE IF WE USE MORE THAN 1 DECK SO IT MIGHT NEED TO BE CHANGED IN THE FUTURE
     hand = list(set(hand))
     print(hand)
 
-
+    
+    #We get the results by callyng on the findBlackjackScore function we defined on test.py
     results = test.findBlackjackScore(hand)
     print(results)
+    
+    #This if/else statement checks if the result is greater than 21, if so we print "BUST"" otherwise the score is printed
     if results <= 21:
         cvzone.putTextRect(img, f'{results} ', (20, 50), scale=1.8)
     else:
